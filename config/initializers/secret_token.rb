8e2876c76fbdde0e7fa7065f1a7f0e32a46bae05
Nadine Eter <nadine_eter@hotmail.de>
2014-01-14 13:46:34 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Haushaltsbuch::Application.config.secret_key_base = 'ca3bb01fe20f4213735005b04a7033b042f54cf96d46cd33874b627c0825075ee868c9d2837403211ea65e64b59a060a60f70cd922a58493a44017d1ae6c14c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Haushaltsbuch::Application.config.session_store :cookie_store, key: '_haushaltsbuch_session'
